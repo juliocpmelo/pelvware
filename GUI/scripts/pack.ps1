410d3e1ea1f3c464c1045c532d4674b6c58bdf1f
julio <julio@solarz.com.br>
2024-01-11 19:54:47 -0300

    fix. adding another hiden import.

--- GUI/scripts/pack.ps1
+++ GUI/scripts/pack.ps1
@@ -1,4 +1,5 @@
 pyinstaller.exe .\MainGUI.py -F --onefile `
                 --hidden-import pyqtgraph.graphicsItems.ViewBox.axisCtrlTemplate_pyqt5 `
                 --hidden-import pyqtgraph.graphicsItems.PlotItem.plotConfigTemplate_pyqt5 `
+                --hidden-import pyqtgraph.GraphicsScene.exportDialogTemplate_pyqt5 `
                 --hidden-import pyqtgraph.imageview.ImageViewTemplate_pyqt5
